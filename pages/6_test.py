35c165cbfbf1340eb54b1921d00e8d73c2b16424
Bennett Paradis <60286771+bennettparadis@users.noreply.github.com>
2024-06-29 20:40:13 -0400

    Create 6_test.py

---
+++ pages/6_test.py
@@ -0,0 +1,59 @@
+import streamlit as st
+import pandas as pd
+import pydeck as pdk
+import geopandas as gpd
+
+# Load data
+df = pd.read_csv('data/2019-2023_oyster_densities.csv')
+
+default_year = 2023
+default_year_index = list(df["Year"].unique()).index(default_year)
+
+year = st.sidebar.selectbox(
+    "Select a Year:", 
+    df["Year"].unique(),
+    index=default_year_index,
+    key=30
+)
+
+df_selection = df[df["Year"] == year]
+
+max_total = df_selection['total'].max()
+df_selection['tooltip'] = df_selection['total'].apply(lambda x: f'{x} oysters/m²')
+
+density_layer = pdk.Layer(
+    "HexagonLayer",
+    data=df_selection,
+    get_position=["Longitude", "Latitude"],
+    radius=8,  
+    elevation_scale=1,  
+    elevation_range=[0, 3000],
+    extruded=True,
+    pickable=True,
+    get_elevation="total",
+    auto_highlight=True,
+    get_fill_color="[255, total * 5, total * 5]",
+)
+
+tooltip = {
+    "html": "<b>Oysters/m²:</b> {total}",
+    "style": {
+        "backgroundColor": "steelblue",
+        "color": "white"
+    }
+}
+
+# Display map
+st.pydeck_chart(
+    pdk.Deck(
+        map_style="mapbox://styles/mapbox/outdoors-v9",
+        initial_view_state={
+            "latitude": 35.05,
+            "longitude": -76.4,
+            "zoom": 11.2,
+            "pitch": 60,
+        },
+        layers=[density_layer],  # Start with only these two layers
+        tooltip=tooltip
+    )
+)
