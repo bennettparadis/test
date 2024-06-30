fe08b2d530b77c7d40c9a59e439dc59f822733ab
Bennett Paradis <60286771+bennettparadis@users.noreply.github.com>
2024-06-29 22:04:02 -0400

    Update 1_📋Methodology.py

--- pages/1_📋Methodology.py
+++ pages/1_📋Methodology.py
@@ -4,10 +4,10 @@ import streamlit as st
 custom_css = """
 <style>
     /* Change the color and font of the page titles in the sidebar */
-    .eczjsme1 {
+    .eczjsme13 {
         color: #00647B !important; /* Replace with your desired color */
         font-weight: bold !important;
-        font-size: 18px !important;
+        font-size: 16px !important;
     }
 </style>
 """
